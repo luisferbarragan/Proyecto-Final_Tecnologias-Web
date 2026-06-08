94d1ab7782b12a8d14d99889d5e0fc851cd6ab8a
luisferbarragan <luisfer1918650@gmail.com>
2026-06-08 00:32:55 -0600

    Se creo el contexto global para manejar los datos del CV

---
+++ src/context/CVContext.jsx
@@ -0,0 +1,75 @@
+import { createContext, useContext, useState } from 'react'
+
+const CVContext = createContext()
+
+const initialCVData = {
+  personalInfo: {
+    fullName: '',
+    profession: '',
+    location: '',
+    email: '',
+    phone: '',
+    profile: '',
+    github: '',
+    linkedin: '',
+    portfolio: '',
+    profileImage: '',
+  },
+  skills: [],
+  projects: [],
+  education: [],
+}
+
+export function CVProvider({ children }) {
+  const [cvData, setCVData] = useState(initialCVData)
+
+  const updatePersonalInfo = (field, value) => {
+    setCVData((prevData) => ({
+      ...prevData,
+      personalInfo: {
+        ...prevData.personalInfo,
+        [field]: value,
+      },
+    }))
+  }
+
+  const addSkill = (skill) => {
+    setCVData((prevData) => ({
+      ...prevData,
+      skills: [...prevData.skills, skill],
+    }))
+  }
+
+  const addProject = (project) => {
+    setCVData((prevData) => ({
+      ...prevData,
+      projects: [...prevData.projects, project],
+    }))
+  }
+
+  const addEducation = (educationItem) => {
+    setCVData((prevData) => ({
+      ...prevData,
+      education: [...prevData.education, educationItem],
+    }))
+  }
+
+  return (
+    <CVContext.Provider
+      value={{
+        cvData,
+        setCVData,
+        updatePersonalInfo,
+        addSkill,
+        addProject,
+        addEducation,
+      }}
+    >
+      {children}
+    </CVContext.Provider>
+  )
+}
+
+export function useCV() {
+  return useContext(CVContext)
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { CVProvider } from './context/CVContext.jsx'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CVProvider>
+        <App />
+      </CVProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
